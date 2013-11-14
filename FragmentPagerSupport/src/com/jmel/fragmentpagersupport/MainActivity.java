e804fc590f2305a5bd998b4df4fa8b210855f6ed
jessemelamed <jmelamed4@gmail.com>
2013-11-14 11:09:40 -0800

    first test with new implemented tabs code seeing what happens

--- FragmentPagerSupport/src/com/jmel/fragmentpagersupport/MainActivity.java
+++ FragmentPagerSupport/src/com/jmel/fragmentpagersupport/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jmel.fragmentpagersupport;
 
+import java.util.ArrayList;
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -32,7 +36,7 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_pager);
 
-        mAdapter = new MyAdapter(getSupportFragmentManager());
+        //mAdapter = new MyAdapter(getSupportFragmentManager(), this, mPager);
 
         mPager = (ViewPager)findViewById(R.id.pager);
         mPager.setAdapter(mAdapter);
@@ -53,10 +57,25 @@ public class MainActivity extends FragmentActivity {
         });
         
         
-        ActionBar actionBar = getActionBar();
+        final ActionBar bar = getActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
 
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        // Create Home Tab
+        mAdapter = new MyAdapter(getSupportFragmentManager(), this, mPager);
+        mAdapter.addTab(bar.newTab().setText("Simple"),
+                HomePageFragment.class, null);
+        mAdapter.addTab(bar.newTab().setText("List"),
+                VotingListFragment.class, null);
+        mAdapter.addTab(bar.newTab().setText("Cursor"),
+                SongListFragment.class, null);
+
+        if (savedInstanceState != null) {
+            bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+        }
+        /*ActionBar actionBar = getActionBar();
+
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);*/
+        /*// Create Home Tab
         Tab tab = actionBar.newTab().setTabListener(new HomePageFragment());
         // Set Tab Title
         tab.setText("Home");
@@ -72,37 +91,94 @@ public class MainActivity extends FragmentActivity {
         tab = actionBar.newTab().setTabListener(new SongListFragment());
         // Set Tab Title
         tab.setText("Songs");
-        actionBar.addTab(tab);
+        actionBar.addTab(tab);*/
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
     }
 
-    public static class MyAdapter extends FragmentPagerAdapter {
-        public MyAdapter(FragmentManager fm) {
+    public static class MyAdapter extends FragmentPagerAdapter implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
+       // public MyAdapter(FragmentManager fm) {
+       //     super(fm);
+       // }
+        
+        private final Context mContext;
+        private final ActionBar mActionBar;
+        private final ViewPager mViewPager;
+        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+
+        static final class TabInfo {
+            private final Class<?> clss;
+            private final Bundle args;
+
+            TabInfo(Class<?> _class, Bundle _args) {
+                clss = _class;
+                args = _args;
+            }
+        }
+        
+        public MyAdapter(FragmentManager fm, Activity activity, ViewPager pager) {
             super(fm);
+            mContext = activity;
+            mActionBar = activity.getActionBar();
+            mViewPager = pager;
+            mViewPager.setAdapter(this);
+            mViewPager.setOnPageChangeListener(this);
         }
 
-        @Override
-        public int getCount() {
-            return NUM_ITEMS;
+        public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
+            TabInfo info = new TabInfo(clss, args);
+            tab.setTag(info);
+            tab.setTabListener(this);
+            mTabs.add(info);
+            mActionBar.addTab(tab);
+            notifyDataSetChanged();
         }
 
-        
-        @Override
-        public Fragment getItem(int position) {
-        
-        	if(position == 0){
-        		//home page fragment
-        		return HomePageFragment.newInstance(position);
-        	}
-        	else if(position == 1){
-        		//voting page fragment
-        		return VotingListFragment.newInstance(position);
-        	}
-        	else if(position == 2){
-        		return SongListFragment.newInstance(position);
-        	}
-        	else
-        		return HomePageFragment.newInstance(position);
-        }
+            @Override
+            public int getCount() {
+                return mTabs.size();
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                TabInfo info = mTabs.get(position);
+                return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mActionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+
+            @Override
+            public void onTabSelected(Tab tab, FragmentTransaction ft) {
+                Object tag = tab.getTag();
+                for (int i=0; i<mTabs.size(); i++) {
+                    if (mTabs.get(i) == tag) {
+                        mViewPager.setCurrentItem(i);
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+            }
+
+            @Override
+            public void onTabReselected(Tab tab, FragmentTransaction ft) {
+            }
     }
     
     public static class HomePageFragment extends ListFragment implements ActionBar.TabListener {
