ca8f0509681a47dbd5b1746b9ed10aa1694bf992
jessemelamed <jmelamed4@gmail.com>
2013-11-26 18:32:40 -0800

    jesses working code with search and radiobutton voting before merging with alex.

--- FragmentPagerSupport/gen/com/jmel/fragmentpagersupport/R.java
+++ FragmentPagerSupport/gen/com/jmel/fragmentpagersupport/R.java
@@ -26,17 +26,18 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_search=0x7f090009;
-        public static final int action_settings=0x7f09000a;
+        public static final int action_search=0x7f09000a;
+        public static final int action_settings=0x7f09000b;
         public static final int ip1=0x7f090000;
         public static final int ip2=0x7f090001;
         public static final int ip3=0x7f090002;
         public static final int ip4=0x7f090003;
         public static final int pager=0x7f090005;
         public static final int port=0x7f090004;
-        public static final int songeditText=0x7f090007;
-        public static final int submit_vote=0x7f090008;
-        public static final int text=0x7f090006;
+        public static final int progress_bar=0x7f090006;
+        public static final int songeditText=0x7f090008;
+        public static final int submit_vote=0x7f090009;
+        public static final int text=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- FragmentPagerSupport/src/com/jmel/fragmentpagersupport/MainActivity.java
+++ FragmentPagerSupport/src/com/jmel/fragmentpagersupport/MainActivity.java
@@ -30,24 +30,26 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 //import java.io.OutputStream;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity{// implements ProgressBar{
 	static final int NUM_ITEMS = 3;
 
 	MyAdapter mAdapter;
 
 	ViewPager mPager;
-	
+
+	// private ProgressBar mProgress;
+	// private int mProgressStatus = 0;
+	// private Handler mHandler = new Handler();
+	// ProgressBar progress_bar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -80,29 +82,52 @@ public class MainActivity extends FragmentActivity {
 				.detectDiskReads().detectDiskWrites().detectNetwork()
 				.penaltyLog().build());
 
-		// Watch for button clicks.
-		
-		/*Button button = (Button) findViewById(R.id.submit_vote);
-		button.setOnClickListener(new OnClickListener() { */
-
-		  //});
-		  
-		  
-			/*private void sendVote() {
-				VotingListFragment votefragvar = new VotingListFragment();
-				long id = votefragvar.songid;
-				//MainActivity a = (MainActivity) getActivity();
-				MyApplication app = (MyApplication) getApplication();
-				app.sendMessage((int) id);
-				
-			}*/
-
-		// Set up a timer task. We will use the timer to check the
-		// input queue every 500 ms
-
 		TCPReadTimerTask tcp_task = new TCPReadTimerTask();
 		Timer tcp_timer = new Timer();
 		tcp_timer.schedule(tcp_task, 3000, 500);
+		
+        /*TextProgressBar textProgressBar = (TextProgressBar) findViewById(R.id.progress_bar);
+        textProgressBar.setText("Loading 70%");
+        textProgressBar.setProgress(70);
+        textProgressBar.setTextSize(18);*/
+
+		// progress_bar = new ProgressBar(this);
+		// mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+
+		/*
+		 * // Start lengthy operation in a background thread int time = 0;
+		 * while(time != 6){ time++; new Thread(new Runnable() { public void
+		 * run() { while (mProgressStatus < 100) { int i = 0; try {
+		 * Thread.sleep(10000); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); } i += 10;
+		 * mProgressStatus = i;
+		 * 
+		 * // Update the progress bar mHandler.post(new Runnable() { public void
+		 * run() { mProgress.setProgress(mProgressStatus); } }); } } }).start();
+		 * } Thread.currentThread().isInterrupted();
+		 * 
+		 * publishprogress(0);
+		 */
+		// Watch for button clicks.
+
+		/*
+		 * Button button = (Button) findViewById(R.id.submit_vote);
+		 * button.setOnClickListener(new OnClickListener() {
+		 */
+
+		// });
+
+		/*
+		 * private void sendVote() { VotingListFragment votefragvar = new
+		 * VotingListFragment(); long id = votefragvar.songid; //MainActivity a
+		 * = (MainActivity) getActivity(); MyApplication app = (MyApplication)
+		 * getApplication(); app.sendMessage((int) id);
+		 * 
+		 * }
+		 */
+
+		// Set up a timer task. We will use the timer to check the
+		// input queue every 500 ms
 	}
 
 	@Override
@@ -120,9 +145,53 @@ public class MainActivity extends FragmentActivity {
 		outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
 	}
 
+	/*public class TextProgressBar extends ProgressBar {
+
+		private String text = "";
+		private int textColor = Color.BLACK;
+		private float textSize = 15;
+
+		public TextProgressBar(Context context) {
+			super(context);
+		}
+
+		@Override
+		protected synchronized void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+			// create an instance of class Paint, set color and font size
+			Paint textPaint = new Paint();
+			textPaint.setAntiAlias(true);
+			textPaint.setColor(textColor);
+			textPaint.setTextSize(textSize);
+			// In order to show text in a middle, we need to know its size
+			Rect bounds = new Rect();
+			textPaint.getTextBounds(text, 0, text.length(), bounds);
+			// Now we store font size in bounds variable and can calculate it's
+			// position
+			int x = getWidth() / 2 - bounds.centerX();
+			int y = getHeight() / 2 - bounds.centerY();
+			// drawing text with appropriate color and size in the center
+			canvas.drawText(text, x, y, textPaint);
+		}
+
+		public synchronized void setText(String text) {
+			if (text != null) {
+				this.text = text;
+			} else {
+				this.text = "";
+			}
+			postInvalidate();
+		}
+		
+	    public synchronized void setTextSize(float textSize) {
+	        this.textSize = textSize;
+	        postInvalidate();
+	    }
+
+	}*/
+
 	public static class MyAdapter extends FragmentPagerAdapter implements
 			ActionBar.TabListener, ViewPager.OnPageChangeListener {
-
 
 		private final Context mContext;
 		private final ActionBar mActionBar;
@@ -269,8 +338,6 @@ public class MainActivity extends FragmentActivity {
 		String pagename;
 		long songid;
 
-		 
-
 		/**
 		 * Create a new instance of CountingFragment, providing "num" as an
 		 * argument.
@@ -319,33 +386,37 @@ public class MainActivity extends FragmentActivity {
 			}
 			super.onActivityCreated(savedInstanceState);
 			setListAdapter(new ArrayAdapter<String>(getActivity(),
-					android.R.layout.simple_list_item_1, votinglist));
+					android.R.layout.simple_list_item_single_choice, votinglist));
 		}
 
 		@Override
 		public void onListItemClick(ListView l, View v, int position, long id) {
 			Log.i("VotingPageFragmentList", "Item clicked: " + id);
-			
+
 			this.songid = id;
-			//MainActivity a = (MainActivity) getActivity();
-			//MyApplication app = (MyApplication) a.getApplication();
-			//app.sendMessage((int) id);
+	         ListView listView = getListView();
+	         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	         listView.setSelection(0);
+	         listView.setSelected(true);
+			// MainActivity a = (MainActivity) getActivity();
+			// MyApplication app = (MyApplication) a.getApplication();
+			// app.sendMessage((int) id);
 		}
-		
-		public void submitVote(View view){ 
+
+		public void submitVote(View view) {
 			VotingListFragment votefragvar = new VotingListFragment();
 			long id = votefragvar.songid;
 			MainActivity a = (MainActivity) getActivity();
 			MyApplication app = (MyApplication) a.getApplication();
-			app.sendMessage((int) id); 
-		 // Context context = a.getApplicationContext();
-		  //CharSequence text = "+id";
-		 // int duration = Toast.LENGTH_SHORT;
+			app.sendMessage((int) id);
+			// Context context = a.getApplicationContext();
+			// CharSequence text = "+id";
+			// int duration = Toast.LENGTH_SHORT;
 
-		 // Toast toast = Toast.makeText(context, text, duration);
-		  //toast.show();
+			// Toast toast = Toast.makeText(context, text, duration);
+			// toast.show();
 
-	  }
+		}
 
 		@Override
 		public void onTabSelected(Tab tab, FragmentTransaction ft) {
@@ -366,12 +437,14 @@ public class MainActivity extends FragmentActivity {
 	}
 
 	public static class SongListFragment extends ListFragment implements
-			ActionBar.TabListener{//, TextWatcher {
+			ActionBar.TabListener {// , TextWatcher {
 		int mNum;
 		String pagename;
-		//Fragment fm = (Fragment) getFragmentManager().findFragmentById(R.id.SongListFragment);
-		//FragmentManager.findFragmentById(R.id.songeditText);
-		EditText et;// = (EditText) getListView().findViewById(R.id.songeditText);
+		// Fragment fm = (Fragment)
+		// getFragmentManager().findFragmentById(R.id.SongListFragment);
+		// FragmentManager.findFragmentById(R.id.songeditText);
+		EditText et;// = (EditText)
+					// getListView().findViewById(R.id.songeditText);
 
 		/**
 		 * Create a new instance of CountingFragment, providing "num" as an
@@ -410,19 +483,19 @@ public class MainActivity extends FragmentActivity {
 			View tv = v.findViewById(R.id.text);
 			((TextView) tv).setText("Song List");
 
-			//final ListView lv = (ListView) v.findViewById(R.id.1); //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-			final ArrayList<String> ar = new ArrayList<String>();
+			// final ListView lv = (ListView) v.findViewById(R.id.1);
+			// //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// final ArrayList<String> ar = new ArrayList<String>();
 			et = (EditText) v.findViewById(R.id.songeditText);
-			//final String[] words = list.TERM;
+			// final String[] words = list.TERM;
 			// Populate list with our static array of titles.
-			//lv.setAdapter(new ArrayAdapter(getActivity(),
-				//	android.R.layout.simple_list_item_activated_1, songs));
+			// lv.setAdapter(new ArrayAdapter(getActivity(),
+			// android.R.layout.simple_list_item_activated_1, songs));
 			setListAdapter(new ArrayAdapter<String>(getActivity(),
 					android.R.layout.simple_list_item_activated_1, songs));
-			//setTextFilterEnabled(true);
-			
-			et = (EditText) v.findViewById(R.id.songeditText);
+			// setTextFilterEnabled(true);
 
+			et = (EditText) v.findViewById(R.id.songeditText);
 
 			et.addTextChangedListener(new TextWatcher() {
 				public void afterTextChanged(Editable s) {
@@ -445,13 +518,6 @@ public class MainActivity extends FragmentActivity {
 									et.getText().toString().toLowerCase())) {
 								songs_sort.add(songs[i]);
 							}
-							/*if (et.getText()
-									.toString()
-									.equalsIgnoreCase(
-											(String) songs[i].subSequence(0,
-													textlength))) {
-								songs_sort.add(songs[i]);
-							}*/
 						}
 					}
 
@@ -463,12 +529,12 @@ public class MainActivity extends FragmentActivity {
 			return v;
 		}
 
-		final String[] songs = new String[20];
+		final String[] songs = new String[100];
 
 		@Override
 		public void onActivityCreated(Bundle savedInstanceState) {
 
-			for (int i = 0; i < 20; i++) {
+			for (int i = 0; i < 100; i++) {
 				songs[i] = "song " + Integer.toString(i);
 			}
 			super.onActivityCreated(savedInstanceState);
@@ -482,9 +548,8 @@ public class MainActivity extends FragmentActivity {
 
 			MainActivity a = (MainActivity) getActivity();
 			MyApplication app = (MyApplication) a.getApplication();
-			app.sendMessage((int) id);
+			//app.sendMessage((int) id);
 		}
-
 
 		@Override
 		public void onTabSelected(Tab tab, FragmentTransaction ft) {
