38d5308f8ee0738c4a9c58a304ac73bcde60e573
justinsiu <justin.siu.0913@gmail.com>
2013-11-30 01:31:27 -0800

    Fixed Facebook Integration

--- FragmentPagerSupport/src/com/jmel/fragmentpagersupport/MainActivity.java
+++ FragmentPagerSupport/src/com/jmel/fragmentpagersupport/MainActivity.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -29,8 +28,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewPager;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -46,9 +43,7 @@ import android.widget.Toast;
 
 import com.facebook.FacebookRequestError;
 import com.facebook.HttpMethod;
-import com.facebook.RequestAsyncTask;
 import com.facebook.Response;
-import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.widget.LoginButton;
 //import java.io.OutputStream;
@@ -64,6 +59,9 @@ public class MainActivity extends FragmentActivity implements  SongListFragment.
 
 	ViewPager mPager;
 
+	//FB
+	private HomePageFragment homePageFragment;
+	
 	// private ProgressBar mProgress;
 	// private int mProgressStatus = 0;
 	// private Handler mHandler = new Handler();
@@ -111,6 +109,20 @@ public class MainActivity extends FragmentActivity implements  SongListFragment.
 
 		// Set up a timer task. We will use the timer to check the
 		// input queue every 500 ms
+		
+		/*//FB
+		if (savedInstanceState == null) {
+	        // Add the fragment on initial activity setup
+	        homePageFragment = new HomePageFragment();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, homePageFragment)
+	        .commit();
+	    } else {
+	        // Or set the fragment from restored state info
+	        homePageFragment = (HomePageFragment) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+	    }*/
 	}
 
 	public void OnSongSelected(String string[]) {
@@ -227,13 +239,12 @@ public class MainActivity extends FragmentActivity implements  SongListFragment.
 			ActionBar.TabListener {
 		int mNum;
 		String pagename;
-
-		// FB
+		
+		//FB
+		private static final String TAG = "MainFragment";
 		private UiLifecycleHelper uiHelper;
-		private static final String TAG = "HomePageFragment";
 		private Button shareButton;
-		private static final List<String> PERMISSIONS = Arrays
-				.asList("publish_actions");
+		private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
 		private static final String PENDING_PUBLISH_KEY = "pendingPublishReauthorization";
 		private boolean pendingPublishReauthorization = false;
 
@@ -259,8 +270,10 @@ public class MainActivity extends FragmentActivity implements  SongListFragment.
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+			
+			//FB
 			uiHelper = new UiLifecycleHelper(getActivity(), callback);
-			uiHelper.onCreate(savedInstanceState);
+		    uiHelper.onCreate(savedInstanceState);
 
 		}
 
@@ -276,25 +289,152 @@ public class MainActivity extends FragmentActivity implements  SongListFragment.
 			// View tv = v.findViewById(R.id.text);
 			// ((TextView)tv).setText( "HomePage" );
 
-			// FB
-			LoginButton authButton = (LoginButton) v
-					.findViewById(R.id.authButton);
-			authButton.setFragment(this);
-			authButton.setReadPermissions(Arrays.asList("user_likes",
-					"user_status"));
-			shareButton = (Button) v.findViewById(R.id.shareButton);
-			shareButton.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					publishStory();
-				}
-			});
-			if (savedInstanceState != null) {
-				pendingPublishReauthorization = savedInstanceState.getBoolean(
-						PENDING_PUBLISH_KEY, false);
-			}
-
+			//FB
+			LoginButton authButton = (LoginButton) v.findViewById(R.id.authButton);
+		    authButton.setFragment(this);
+		    //authButton.setReadPermissions(Arrays.asList("user_likes", "user_status"));
+		    shareButton = (Button) v.findViewById(R.id.shareButton);
+		    shareButton.setOnClickListener(new View.OnClickListener() {
+		        @Override
+		        public void onClick(View v) {
+		            publishStory();        
+		        }
+		    });
+		    if (savedInstanceState != null) {
+		        pendingPublishReauthorization = 
+		            savedInstanceState.getBoolean(PENDING_PUBLISH_KEY, false);
+		    }
+		    
 			return v;
+		}
+		
+		//FB
+		private void onSessionStateChange(com.facebook.Session session, com.facebook.SessionState state, Exception exception) {
+		    if (state.isOpened()) {
+		        Log.i(TAG, "Logged in...");
+		        shareButton.setVisibility(View.VISIBLE);
+		        if (pendingPublishReauthorization && 
+		                state.equals(com.facebook.SessionState.OPENED_TOKEN_UPDATED)) {
+		            pendingPublishReauthorization = false;
+		            publishStory();
+		        }
+		    } else if (state.isClosed()) {
+		        Log.i(TAG, "Logged out...");
+		        shareButton.setVisibility(View.INVISIBLE);
+		    }
+		}
+		
+		private com.facebook.Session.StatusCallback callback = new com.facebook.Session.StatusCallback() {
+		    @Override
+		    public void call(com.facebook.Session session, com.facebook.SessionState state, Exception exception) {
+		        onSessionStateChange(session, state, exception);
+		    }
+		};
+		
+		@Override
+		public void onResume() {
+		    super.onResume();
+		 // For scenarios where the main activity is launched and user
+		    // session is not null, the session state change notification
+		    // may not be triggered. Trigger it if it's open/closed.
+		    com.facebook.Session session = com.facebook.Session.getActiveSession();
+		    if (session != null &&
+		           (session.isOpened() || session.isClosed()) ) {
+		        onSessionStateChange(session, session.getState(), null);
+		    }
+		    uiHelper.onResume();
+		}
+
+		@Override
+		public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		    super.onActivityResult(requestCode, resultCode, data);
+		    uiHelper.onActivityResult(requestCode, resultCode, data);
+		}
+
+		@Override
+		public void onPause() {
+		    super.onPause();
+		    uiHelper.onPause();
+		}
+
+		@Override
+		public void onDestroy() {
+		    super.onDestroy();
+		    uiHelper.onDestroy();
+		}
+
+		@Override
+		public void onSaveInstanceState(Bundle outState) {
+		    super.onSaveInstanceState(outState);
+		    outState.putBoolean(PENDING_PUBLISH_KEY, pendingPublishReauthorization);
+		    uiHelper.onSaveInstanceState(outState);
+		}
+		
+		private void publishStory() {
+		    com.facebook.Session session = com.facebook.Session.getActiveSession();
+
+		    if (session != null){
+
+		        // Check for publish permissions    
+		        List<String> permissions = session.getPermissions();
+		        if (!isSubsetOf(PERMISSIONS, permissions)) {
+		            pendingPublishReauthorization = true;
+		            com.facebook.Session.NewPermissionsRequest newPermissionsRequest = new com.facebook.Session
+		                    .NewPermissionsRequest(this, PERMISSIONS);
+		        session.requestNewPublishPermissions(newPermissionsRequest);
+		            return;
+		        }
+
+		        Bundle postParams = new Bundle();
+		        postParams.putString("name", "Facebook SDK for Android");
+		        postParams.putString("caption", "Build great social apps and get more installs.");
+		        postParams.putString("description", "The Facebook SDK for Android makes it easier and faster to develop Facebook integrated Android apps.");
+		        postParams.putString("link", "https://developers.facebook.com/android");
+		        postParams.putString("picture", "https://raw.github.com/fbsamples/ios-3.x-howtos/master/Images/iossdk_logo.png");
+
+		        com.facebook.Request.Callback callback= new com.facebook.Request.Callback() {
+		            public void onCompleted(Response response) {
+		                JSONObject graphResponse = response
+		                                           .getGraphObject()
+		                                           .getInnerJSONObject();
+		                String postId = null;
+		                try {
+		                    postId = graphResponse.getString("id");
+		                } catch (JSONException e) {
+		                    Log.i(TAG,
+		                        "JSON error "+ e.getMessage());
+		                }
+		                FacebookRequestError error = response.getError();
+		                if (error != null) {
+		                    Toast.makeText(getActivity()
+		                         .getApplicationContext(),
+		                         error.getErrorMessage(),
+		                         Toast.LENGTH_SHORT).show();
+		                    } else {
+		                        Toast.makeText(getActivity()
+		                             .getApplicationContext(), 
+		                             postId,
+		                             Toast.LENGTH_LONG).show();
+		                }
+		            }
+		        };
+
+		        com.facebook.Request request = new com.facebook.Request(session, "me/feed", postParams, 
+		                              HttpMethod.POST, callback);
+
+		        com.facebook.RequestAsyncTask task = new com.facebook.RequestAsyncTask(request);
+		        task.execute();
+		    }
+
+		}
+		
+		private boolean isSubsetOf(Collection<String> subset, Collection<String> superset) {
+		    for (String string : subset) {
+		        if (!superset.contains(string)) {
+		            return false;
+		        }
+		    }
+		    return true;
 		}
 
 		@Override
@@ -313,155 +453,7 @@ public class MainActivity extends FragmentActivity implements  SongListFragment.
 
 		}
 
-		// FB Session
-		private void onSessionStateChange(com.facebook.Session session,
-				com.facebook.SessionState state, Exception exception) {
-			if (state.isOpened()) {
-				Log.i(TAG, "Logged in...");
-			} else if (state.isClosed()) {
-				Log.i(TAG, "Logged out...");
-			}
-			if (state.isOpened()) {
-				shareButton.setVisibility(View.VISIBLE);
-				if (pendingPublishReauthorization
-						&& state.equals(SessionState.OPENED_TOKEN_UPDATED)) {
-					pendingPublishReauthorization = false;
-					publishStory();
-				}
-			} else if (state.isClosed()) {
-				shareButton.setVisibility(View.INVISIBLE);
-			}
-		}
-
-		private com.facebook.Session.StatusCallback callback = new com.facebook.Session.StatusCallback() {
-			@Override
-			public void call(com.facebook.Session session,
-					com.facebook.SessionState state, Exception exception) {
-				onSessionStateChange(session, state, exception);
-			}
-		};
-
-		@Override
-		public void onResume() {
-			super.onResume();
-			com.facebook.Session session = com.facebook.Session
-					.getActiveSession();
-			if (session != null && (session.isOpened() || session.isClosed())) {
-				onSessionStateChange(session, session.getState(), null);
-			}
-			uiHelper.onResume();
-		}
-
-		@Override
-		public void onActivityResult(int requestCode, int resultCode,
-				Intent data) {
-			super.onActivityResult(requestCode, resultCode, data);
-			uiHelper.onActivityResult(requestCode, resultCode, data);
-		}
-
-		@Override
-		public void onPause() {
-			super.onPause();
-			uiHelper.onPause();
-		}
-
-		@Override
-		public void onDestroy() {
-			super.onDestroy();
-			uiHelper.onDestroy();
-		}
-
-		@Override
-		public void onSaveInstanceState(Bundle outState) {
-			super.onSaveInstanceState(outState);
-			outState.putBoolean(PENDING_PUBLISH_KEY,
-					pendingPublishReauthorization);
-			uiHelper.onSaveInstanceState(outState);
-		}
-
-		private void publishStory() {
-			com.facebook.Session session = com.facebook.Session
-					.getActiveSession();
-
-			if (session != null) {
-
-				// Check for publish permissions
-				List<String> permissions = session.getPermissions();
-				if (!isSubsetOf(PERMISSIONS, permissions)) {
-					pendingPublishReauthorization = true;
-					com.facebook.Session.NewPermissionsRequest newPermissionsRequest = new com.facebook.Session.NewPermissionsRequest(
-							this, PERMISSIONS);
-					session.requestNewPublishPermissions(newPermissionsRequest);
-					return;
-				}
-
-				Bundle postParams = new Bundle();
-
-				MainActivity a = (MainActivity) getActivity();
-				SongListFragment fragment = (SongListFragment) a.getAdapter()
-						.getItem(2);
-
-				long id = fragment.songid;
-				Log.i("songs", Integer.toString((int) id));
-				Log.i("songs", Integer.toString((int) fragment.songid));
-				postParams.putString("name", "I'm Listening to song " + id
-						+ "!");
-				postParams
-						.putString("caption",
-								"Listen to a song and vote on the next one with your friends!");
-				postParams
-						.putString(
-								"description",
-								"Project created by Jesse Melamed, Alex Sismanis, Justin Siu and Andrew Whitman.");
-				postParams.putString("link",
-						"https://developers.facebook.com/android");
-				postParams
-						.putString("picture",
-								"https://raw.github.com/fbsamples/ios-3.x-howtos/master/Images/iossdk_logo.png");
-
-				com.facebook.Request.Callback callback = new com.facebook.Request.Callback() {
-					public void onCompleted(Response response) {
-						JSONObject graphResponse = response.getGraphObject()
-								.getInnerJSONObject();
-						String postId = null;
-						try {
-							postId = graphResponse.getString("id");
-						} catch (JSONException e) {
-							Log.i(TAG, "JSON error " + e.getMessage());
-						}
-						FacebookRequestError error = response.getError();
-						if (error != null) {
-							Toast.makeText(
-									getActivity().getApplicationContext(),
-									error.getErrorMessage(), Toast.LENGTH_SHORT)
-									.show();
-						} else {
-							Toast.makeText(
-									getActivity().getApplicationContext(),
-									postId, Toast.LENGTH_LONG).show();
-						}
-					}
-				};
-
-				com.facebook.Request request = new com.facebook.Request(
-						session, "me/feed", postParams, HttpMethod.POST,
-						callback);
-
-				RequestAsyncTask task = new RequestAsyncTask(request);
-				task.execute();
-			}
-
-		}
-
-		private boolean isSubsetOf(Collection<String> subset,
-				Collection<String> superset) {
-			for (String string : subset) {
-				if (!superset.contains(string)) {
-					return false;
-				}
-			}
-			return true;
-		}
+		
 	}
 
 	public static class VotingListFragment extends ListFragment implements
